91eac105a8eabbb6f5b2a48103e3f833f86ac931
FedeLopez17 <fedelopez1712@gmail.com>
2022-11-23 16:13:21 -0300

    Style alerts and improve the deletion of a previous alert (if there is one) before showing a new one

--- src/alerts.js
+++ src/alerts.js
@@ -4,7 +4,7 @@ const _ALERT_TIME_UNTIL_FADE_OUT_IN_SECONDS = 2;
 
 export default function displayAlert(alertMessage){
     const previousAlertOnScreen = document.querySelector("#alert");
-    if(previousAlertOnScreen) previousAlertOnScreen.classList.add("hide");
+    if(previousAlertOnScreen) previousAlertOnScreen.remove();
     
     const container = document.querySelector("#content");
     const alert = document.createElement("section");
